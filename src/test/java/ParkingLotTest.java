2fec182d7f0014833f49ba4bef0623659d0e0f2d
Mayank-rathi <Mayankrathi33@gmail.com>
2020-03-22 12:53:33 +0530

    ADDED:Code For UnPark The vehicle

--- src/test/java/ParkingLotTest.java
+++ src/test/java/ParkingLotTest.java
@@ -5,15 +5,22 @@ import org.junit.Test;
 public class ParkingLotTest {
     @Test
     public void giveWelcomeMessage_ForParking_Lot() throws Exception {
-        ParkingLotAnalyser welcomeMessage = new ParkingLotAnalyser();
-        String welcomeCheck = welcomeMessage.getWelcomeMessage();
-        Assert.assertEquals(welcomeCheck, "Welcome");
+        ParkingLotAnalyser parkingLotAnalyser = new ParkingLotAnalyser();
+        String welcomeCheck = parkingLotAnalyser.getWelcomeMessage();
+        Assert.assertEquals("Welcome", welcomeCheck);
     }
 
     @Test
     public void givenParkingLotAnalyser_ForDriverParking_ShouldReturnTrue() {
-        ParkingLotAnalyser checkForParking = new ParkingLotAnalyser();
-        boolean CheckparkingCar = checkForParking.parkingCar(new Object());
+        ParkingLotAnalyser parkingLotAnalyser = new ParkingLotAnalyser();
+        boolean CheckparkingCar = parkingLotAnalyser.parkingCar(new Object());
         Assert.assertTrue("true", CheckparkingCar);
+    }
+
+    @Test
+    public void givenParkingLotAnalyser_ForUnParkingVehical_ShouldReturnTrue() {
+        ParkingLotAnalyser parkingLotAnalyser = new ParkingLotAnalyser();
+        boolean unParkingCar = parkingLotAnalyser.UnParkingCar(new Object());
+        Assert.assertTrue("true", unParkingCar);
     }
 }

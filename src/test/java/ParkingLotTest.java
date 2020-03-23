03e691d336eb8e551e3ae1fff6722a5c8d4b0248
Mayank-rathi <Mayankrathi33@gmail.com>
2020-03-23 16:03:46 +0530

    REFACTORED:Follow Open Close Principle By Implementing Interface

--- src/test/java/ParkingLotTest.java
+++ src/test/java/ParkingLotTest.java
@@ -1,61 +1,100 @@
-import carParkingLot.AirportSecurity;
-import carParkingLot.ParkingLotAnalyser;
-import carParkingLot.ParkingLotOwner;
+import carParkingLot.*;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
-import java.security.Security;
 
 public class ParkingLotTest {
-    @Test
-    public void giveWelcomeMessage_ForParking_Lot() throws Exception {
-        ParkingLotAnalyser parkingLotAnalyser = new ParkingLotAnalyser();
-        String welcomeCheck = parkingLotAnalyser.getWelcomeMessage();
-        Assert.assertEquals("Welcome", welcomeCheck);
+    private static ParkingLotSystem parkingLotSystem;
+    public static Object vehicle, vehicle2, vehicle3;
+
+    @Before
+    public void setUp() throws Exception {
+        parkingLotSystem = new ParkingLotSystem(2);
+        vehicle = new Object();
+        vehicle2 = new Object();
+        vehicle3 = new Object();
     }
 
     @Test
-    public void givenParkingLotAnalyser_ForDriverParking_ShouldReturnTrue() {
-        ParkingLotAnalyser parkingLotAnalyser = new ParkingLotAnalyser();
-        boolean CheckparkingCar = parkingLotAnalyser.parkingCar(new Object());
-        Assert.assertTrue("true", CheckparkingCar);
+    public void givenParkingLot_WhenVehicleIsParked_ShouldReturnTrue() {
+        boolean isParked = parkingLotSystem.parkTheVehicle(vehicle);
+        Assert.assertTrue(isParked);
     }
 
     @Test
-    public void givenParkingLotAnalyser_ForUnParkingVehical_ShouldReturnTrue() {
-        ParkingLotAnalyser parkingLotAnalyser = new ParkingLotAnalyser();
-        boolean unParkingCar = parkingLotAnalyser.UnParkingCar(new Object());
-        Assert.assertTrue("true", unParkingCar);
-    }
-
-    //Check For Owner
-    @Test
-    public void givenParkingLot_CheckForOwner_IfFull_ShouldReturnTrue() {
-        ParkingLotOwner parkingLotOwner = new ParkingLotOwner();
-        boolean checkForLot = parkingLotOwner.checkForLotSize(new Object());
-        Assert.assertEquals(true, checkForLot);
+    public void givenParkingLot_WhenVehicleAlreadyParked_ShouldReturnFalse() {
+        try {
+            parkingLotSystem.parkTheVehicle(vehicle);
+            parkingLotSystem.parkTheVehicle(vehicle);
+        } catch (ParkingLotException e) {
+            Assert.assertEquals(ParkingLotException.ExceptionType.VEHICLE_IS_ALREADY_PARK, e.type);
+        }
     }
 
     @Test
-    public void givenParkingLot_CheckForOwner_IfNotFull_ShouldReturnTrue() {
-        ParkingLotOwner parkingLotOwner = new ParkingLotOwner();
-        boolean checkForLot = parkingLotOwner.checkForEmpty(new Object());
-        Assert.assertEquals(true, checkForLot);
+    public void givenParkingLot_WhenVehicleIsUnparked_ShouldReturnTrue() {
+        parkingLotSystem.parkTheVehicle(vehicle);
+        boolean isUnparked = parkingLotSystem.UnParking(vehicle);
+        Assert.assertTrue(isUnparked);
     }
 
-    //For Airport Security
+    //Parking-Lot Owner
     @Test
-    public void givenParkingLot_CheckForAirportSecurity_IfSlotIsFull_ShouldReturnTrue() {
-        AirportSecurity airportSecurity = new AirportSecurity();
-        boolean checkForLot = airportSecurity.checkForLotSize(new Object());
-        Assert.assertEquals(true, checkForLot);
+    public void givenParkingLot_WhenFull_ShouldInfromTheOwner() {
+        ParkingLotOwner owner = new ParkingLotOwner();
+        parkingLotSystem.registredParkingLotObserver(owner);
+        try {
+            parkingLotSystem.parkTheVehicle(vehicle);
+            parkingLotSystem.parkTheVehicle(vehicle2);
+            parkingLotSystem.parkTheVehicle(vehicle3);
+        } catch (ParkingLotException e) {
+        }
+        boolean isCapacityFull = owner.isCapacityFull();
+        Assert.assertTrue(isCapacityFull);
     }
 
     @Test
-    public void givenParkingLot_CheckForAirportSecurity_IfSlotIsNotFull_ShouldReturnTrue() {
-        ParkingLotOwner parkingLotOwner = new ParkingLotOwner();
-        boolean checkForLot = parkingLotOwner.checkForEmpty(new Object());
-        Assert.assertEquals(true, checkForLot);
+    public void givenParkingLot_WhenVehiclesAreParkedAndRemoveOneVehicleTheRemainingSpace_ShouldAddAnotherVehicle() {
+        try {
+            parkingLotSystem.parkTheVehicle(vehicle);
+            parkingLotSystem.parkTheVehicle(vehicle2);
+            parkingLotSystem.UnParking(vehicle);
+            boolean isParkedVehicle = parkingLotSystem.parkTheVehicle(vehicle3);
+            Assert.assertTrue(isParkedVehicle);
+        } catch (ParkingLotException e) {
+        }
     }
+
+    //AirPort Security-Staff
+    @Test
+    public void givenParkingLot_WhenLotIsFull_ShouldInfromAirPortSecurityStaff() {
+        AirPortSecurityStaff airPortSecurity = new AirPortSecurityStaff();
+        parkingLotSystem.registredParkingLotObserver(airPortSecurity);
+        try {
+            parkingLotSystem.parkTheVehicle(vehicle);
+            parkingLotSystem.parkTheVehicle(vehicle2);
+            parkingLotSystem.parkTheVehicle(vehicle3);
+        } catch (ParkingLotException e) {
+        }
+        boolean isCapacityFull = airPortSecurity.isCapacityFull();
+        Assert.assertTrue(isCapacityFull);
+    }
+
+    @Test
+    public void givenParkingLot_WhenSpaceAvailable_ShouldReturnTrue() {
+        ParkingLotOwner owner = new ParkingLotOwner();
+        parkingLotSystem.registredParkingLotObserver(owner);
+        try {
+            parkingLotSystem.parkTheVehicle(vehicle);
+            parkingLotSystem.parkTheVehicle(vehicle2);
+            parkingLotSystem.parkTheVehicle(vehicle3);
+        } catch (ParkingLotException e) {
+        }
+        parkingLotSystem.UnParking(vehicle2);
+        boolean spaceAvailable = owner.isCapacityFull();
+        Assert.assertTrue(spaceAvailable);
+    }
+
 }
 
